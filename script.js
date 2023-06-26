030d78bea522f4404db1b059914bc29c23f666da
Vitaliy Voroshnin <vorosninv@gmail.com>
2023-06-26 17:30:31 +0500

    maincontent naw bar test #1

--- script.js
+++ script.js
@@ -29,3 +29,47 @@ const slides = document.querySelectorAll('.switchpic-slide');
         showSlide(activeSlide);
       });
     });
+
+    // получение ссылок на элементы навигации
+const newsLink = document.querySelector('#nav-news');
+const announcementsLink = document.querySelector('#nav-announcements');
+const discussionsLink = document.querySelector('#nav-discussions');
+const articlesLink = document.querySelector('#nav-articles');
+
+// установка класса active для элемента Новости по умолчанию
+newsLink.classList.add('activee');
+
+// добавление обработчиков событий на каждый элемент навигации
+newsLink.addEventListener('click', () => setActive(newsLink));
+announcementsLink.addEventListener('click', () => setActive(announcementsLink));
+discussionsLink.addEventListener('click', () => setActive(discussionsLink));
+articlesLink.addEventListener('click', () => setActive(articlesLink));
+
+// функция для установки активного элемента навигации
+function setActive(clickedElement) {
+  // поиск элемента навигации с активным классом
+  const activeElement = document.querySelector('.activee');
+
+  // удаление класса active у предыдущего активного элемента
+  if (activeElement) {
+    activeElement.classList.remove('activee');
+  }
+
+  // установка класса active для выбранного элемента
+  clickedElement.classList.add('activee');
+}
+
+// функция для инициализации навигации
+function initNavigation() {
+  // установка класса active для элемента Новости по умолчанию
+  newsLink.classList.add('activee');
+
+  // добавление обработчиков событий на каждый элемент навигации
+  newsLink.addEventListener('click', () => setActive(newsLink));
+  announcementsLink.addEventListener('click', () => setActive(announcementsLink));
+  discussionsLink.addEventListener('click', () => setActive(discussionsLink));
+  articlesLink.addEventListener('click', () => setActive(articlesLink));
+}
+
+// вызываем функцию initNavigation для инициализации навигации
+initNavigation();
